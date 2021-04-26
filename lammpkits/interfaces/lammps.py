02ae9f47209d41165c613f6322cdc8ca37e0dd47
kei0822kei <mizokami.keiyu@gmail.com>
2021-04-26 14:38:20 +0900

    Add: Trivial change.

--- lammpkits/interfaces/lammps.py
+++ lammpkits/interfaces/lammps.py
@@ -5,10 +5,12 @@ This module is the interface for PyLammps.
 """
 
 from typing import Union
+import numpy as np
 from lammps import lammps
 
 
-def get_cell_from_lammps(lammps_input:Union[list,lammps]) -> tuple:
+def get_cell_from_lammps(lammps_input:Union[list,lammps],
+                         move_atoms_into_unitcell:bool=True) -> tuple:
     """
     Get cell from lammps input object or strings.
 
@@ -23,8 +25,10 @@ def get_cell_from_lammps(lammps_input:Union[list,lammps]) -> tuple:
     from phonolammps.iofile import get_structure_from_lammps
     from lammpkits.interfaces.phonopy import get_cell_from_phonopy_structure
 
-    cell = get_structure_from_lammps(lammps_input=lammps_input, show_log=True)
-    cell = get_cell_from_phonopy_structure(
-              get_structure_from_lammps(lammps_input))
+    lattice, scaled_positions, symbols = \
+      get_cell_from_phonopy_structure(get_structure_from_lammps(lammps_input))
+    scaled_positions = np.round(scaled_positions, decimals=8)
+    if move_atoms_into_unitcell:
+        scaled_positions %= 1.
 
-    return cell
+    return (lattice, scaled_positions, symbols)
