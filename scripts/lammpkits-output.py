2cf16ac3edf35f4bd0d7a62f31745926c00d7153
kei0822kei <mizokami.keiyu@gmail.com>
2021-07-20 12:12:42 +0900

    Add: Add cui.

--- scripts/lammpkits-output.py
+++ scripts/lammpkits-output.py
@@ -20,8 +20,8 @@ def get_argparse():
         help="Output log.lammps file.")
 
     parser.add_argument(
-        '-k', '--key', type=str, default=None,
-        help="Lammps key shown in log.lammps.")
+        '-k', '--keys', type=str, default=None,
+        help="Lammps keys shown in log.lammps.")
 
     parser.add_argument(
         '-p', '--plot', action='store_true',
@@ -31,23 +31,47 @@ def get_argparse():
 
 
 def main(logfile,
-         key,
+         keys,
          is_plot,
          ):
     lmp_out = LammpsOutput(logfile=logfile)
     print("Load: %s" % logfile)
     print("Available keys are: {}".format(lmp_out.get_keys()))
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    num_of_plots = len(keys)
+    if num_of_plots == 1:
+        l, k = 1, 1
+    elif num_of_plots == 2:
+        l, k = 2, 1
+    elif num_of_plots <= 4:
+        l, k = 2, 2
+    elif num_of_plots <= 6:
+        l, k = 3, 2
+    elif num_of_plots <= 9:
+        l, k = 3, 3
+    elif num_of_plots <= 12:
+        l, k = 4, 3
+    elif num_of_plots <= 16:
+        l, k = 4, 4
+    else:
+        raise RuntimeError("Too many keys (more than 16).")
+
+    fig = plt.figure(figsize=(5*l,5*k))
     if is_plot:
-        ax = lmp_out.plot_transition(ax=ax, key=key)
+        for i, key in enumerate(keys):
+            ax = fig.add_subplot(l,k,i+1)
+            ax = lmp_out.plot_transition(ax=ax, key=key)
         plt.show()
 
 
 if __name__ == '__main__':
     args = get_argparse()
+    if args.keys is not None:
+        keys = list(map(str, args.keys.split()))
+    else:
+        keys = None
+
     main(logfile=args.logfile,
-         key=args.key,
+         keys=keys,
          is_plot=args.plot,
          )
